979623ab9a4697129c0c1819b0dc71c15cfbb473
ElHombreRosa <gabrielrosado0326@gmail.com>
2024-10-25 16:10:23 -0400

    Completed Assignment Project 0-1

--- Project 0-1/CarFinder v0.1.py
+++ Project 0-1/CarFinder v0.1.py
@@ -5,7 +5,9 @@ class CarFinder:
     AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
 
     def __init__(self):
+        
         pass
+    
     def menu_title(self):
         print("********************************")
         print("AutoCountry Vehicle Finder v0.1")
@@ -13,7 +15,7 @@ class CarFinder:
 
     def menu_display(self):
         print("Please Enter the following number below from the following menu:")
-        print("\n1. PRINT all Authorized Vehicles\n2. Exit")
+        print("\n1. PRINT all Authorized Vehicles\n2. Exit\n")
 
     def authorized_vehicles(self):
         print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
@@ -22,20 +24,20 @@ class CarFinder:
 
     def main(self):
         while True: 
-            self.menu_title
-            self.menu_display
+            self.menu_title()
+            self.menu_display()
         
-            selection_choices =  input()
+            selection_choices =  input("Enter Choice Here: ")
         
             if selection_choices == "1":
                 self.authorized_vehicles()
             elif selection_choices == "2":
                 print("\nThank you for using the AutoCountry Vehicle Finder, good-bye!")
-            break
-        else:
-            print("\n Invalid choice, please try again.")
-            input("\n Press 1 or 2 to continue: ")
+                break
+            else:
+                print("\n Invalid choice, please try again.")
+                input("\n Press 1 or 2 to continue: ")
 
 if __name__ == "__main__":
-    car_finder = CarFinder
-    car_finder.run()
+    car_finder = CarFinder()
+    car_finder.main()         
