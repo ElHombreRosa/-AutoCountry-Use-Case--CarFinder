0a49daa302ddc32dc12caab6892f8b7aa51889a7
ElHombreRosa <gabrielrosado0326@gmail.com>
2024-10-25 14:43:48 -0400

    Computer Issue Occur / Unit Test 1

--- Project 0-1/CarFinder v0.1.py
+++ Project 0-1/CarFinder v0.1.py
@@ -1,1 +1,13 @@
 # 10/27/2024
+# CarFinder v0.1
+
+AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
+
+def menu_title():
+    print("********************************")
+    print("AutoCountry Vehicle Finder v0.1")
+    print("********************************")
+
+def menu_display():
+    print("Please Enter the following number below from the following menu:")
+    print("\n1. PRINT all Authorized Vehicles\n2. Exit")
