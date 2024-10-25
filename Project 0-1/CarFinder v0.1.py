aaf362c2c105e0093894c114654402013394d503
ElHombreRosa <gabrielrosado0326@gmail.com>
2024-10-25 14:57:37 -0400

    Unit Test 2 In Progress

--- Project 0-1/CarFinder v0.1.py
+++ Project 0-1/CarFinder v0.1.py
@@ -1,4 +1,3 @@
-# 10/27/2024
 # CarFinder v0.1
 
 AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan' ]
@@ -11,3 +10,8 @@ def menu_title():
 def menu_display():
     print("Please Enter the following number below from the following menu:")
     print("\n1. PRINT all Authorized Vehicles\n2. Exit")
+
+def authorized_vehicles():
+    print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
+    for authorizedvehicles in AllowedVehiclesList:
+        print(authorizedvehicles)
